be8d13fc528d777d422222b31b6813a814e42925
István Donkó <istvan.donko+github@gmail.com>
2017-04-14 19:32:04 +0200

    Updated compilation script

--- scripts/nodemon_compile.ps1
+++ scripts/nodemon_compile.ps1
@@ -1,1 +1,6 @@
-nodemon -e tex --exec pdflatex "-output-directory" "output" "main.tex"
+ param (
+    [string]$main = $( Read-Host "Main file" )
+ )
+
+$name = (split-path $main -Leaf).Split('.')[0]
+nodemon -e tex --exec pdflatex "-output-directory" "output/$name" "compilations/$name.tex"
